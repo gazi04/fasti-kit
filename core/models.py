782de6904ab4a272ea20d9572692c9231676db94
gazi04 <gazmendhalili2016@gmail.com>
2026-07-04 22:07:22 +0200

    :sparkles: Added core/models.py as a manifest for SQLAlchemy models

---
+++ core/models.py
@@ -0,0 +1,7 @@
+"""
+This file acts as a "Manifest" for SQLAlchemy.
+And can be used in the case where we want to import all the models but at the same time keep a clean code.
+"""
+
+from core.database import Base  # noqa: F401
+from user.models import UserModel

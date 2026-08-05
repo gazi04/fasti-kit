02c46398e82490e6e5d92cb151af0d9e9294ac91
gazi04 <gazmendhalili2016@gmail.com>
2026-08-05 22:25:05 +0200

    :art: Reformatted code with ruff

--- core/worker/main.py
+++ core/worker/main.py
@@ -1,9 +1,10 @@
 from core.queue import task_queue
+
 from .tasks import process_welcome_email
 
 # SAQ looks for a dictionary named 'settings' by default
 settings = {
     "queue": task_queue,
     "functions": [process_welcome_email],
-    "concurrency": 10, # Number of jobs to process concurrently
+    "concurrency": 10,  # Number of jobs to process concurrently
 }
--- core/worker/tasks.py
+++ core/worker/tasks.py
@@ -3,11 +3,12 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 async def process_welcome_email(ctx, user_id: int, email: str):
     print(f"\n🚀 WORKER PICKED UP JOB: Sending email to {email} (User {user_id})!\n")
-    
+
     logger.info(f"Starting email job for {email} (User {user_id})")
-    await asyncio.sleep(2) 
+    await asyncio.sleep(2)
     logger.info(f"Successfully sent email to {email}")
-    
+
     return {"status": "delivered", "user_id": user_id}

28db8638fe3ff90d1a2ef5a0ffc2475842559a52
gazi04 <gazmendhalili2016@gmail.com>
2026-08-06 20:11:04 +0200

    :art: Formatted code with ruff

--- core/worker/main.py
+++ core/worker/main.py
@@ -5,8 +5,9 @@ from core.queue import task_queue
 from .tasks import process_welcome_email
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s | WORKER | %(levelname)s | %(message)s",
-    force=True
+    level=logging.INFO,
+    format="%(asctime)s | WORKER | %(levelname)s | %(message)s",
+    force=True,
 )
 
 settings = {
